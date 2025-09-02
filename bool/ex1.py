88ff195b5e643a1ab33ffcc46806345f634a7650
Your Name <you@example.com>
2025-09-02 17:22:26 +0900

    first commit

---
+++ bool/ex1.py
@@ -0,0 +1,7 @@
+# 논리데이터 만들기
+print(True) # 참
+print(False) # 거짓
+
+# 비교 연산자
+print(1 == 1) # 같으면 참
+print(1 != 1) # 다르면 참
---
+++ bool/quiz1.py
@@ -0,0 +1,13 @@
+age = 40
+print(age <= 40)
+
+money = 3000
+print(money >= 2000)
+
+password = "1111"
+print(password != "1234")
+
+x = 19
+y = x % 2
+print(y == 0)
+print((x % 2) == 1)
