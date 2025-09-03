85bdb929d577bf0a4535702ce45d0bcb76f7de7b
Your Name <you@example.com>
2025-09-03 17:10:23 +0900

    09.03

--- if/quiz3.py
+++ if/quiz3.py
@@ -1,10 +1,33 @@
-x = 'p'
+# x = 'b'
 
+# if x == 'b':
+#     print('새')
+# elif x == 'c':
+#     print('고양이')
+# elif x == 'd':
+#     print('강아지')
+# else:
+#     print('none')
+
+# time = 11
+
+# if time == 9:
+#     print('출근')
+# elif time == 10:
+#     print('회의')
+# elif time == 11:
+#     print('서류 정리')
+# elif time == 12:
+#     print('점심 식사')
+# else:
+#     print('휴식')
+
+x = 't'
 if x == 'b':
-    print('새')
+    print('버스')
+elif x == 't':
+    print('지하철')
 elif x == 'c':
-    print('고양이')
-elif x == 'd':
-    print('강아지')
+    print('자전거')
 else:
     print('none')
