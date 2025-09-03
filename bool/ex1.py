3ca3923a92e53c29ae63e01a0086ad81bee5f284
Your Name <you@example.com>
2025-09-03 09:47:58 +0900

    a

--- bool/ex1.py
+++ bool/ex1.py
@@ -3,5 +3,18 @@ print(True) # 참
 print(False) # 거짓
 
 # 비교 연산자
+# 비교연산자는 문자열도 가능
+# 반환값은 bool
 print(1 == 1) # 같으면 참
 print(1 != 1) # 다르면 참
+print("abc" == "abc")  
+print(2 > 1) # 앞의 항이 더 크면 참
+print(2 < 1) # 앞의 항이 더 작으면 참
+print(2 >= 2) # 더 크거나 같으면 참
+print(2 <= 2) # 더 작거나 같으면 참
+
+# 변수를 사용해서 비교하기
+x = 5
+y = 10
+print(x == y) # 5 == 10
+print(x < y) # 5 < 10
--- bool/quiz1.py
+++ bool/quiz1.py
@@ -7,7 +7,7 @@ print(money >= 2000)
 password = "1111"
 print(password != "1234")
 
-x = 19
+x = 4
 y = x % 2
 print(y == 0)
-print((x % 2) == 1)
+print((x % 2) == 0)
